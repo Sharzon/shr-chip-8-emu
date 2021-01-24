cbf97a6c84b682ad08077f1b6e4b34805ff4c14f
Ilia Eliseev <sharzon.soft@gmail.com>
2021-01-24 23:04:20 +0300

    Add execute loop

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,16 @@ fn main() {
         println!("Please, give a file name");
         return;
     }
-    read_file(&args[1]);
+
+    match read_file(&args[1]) {
+        Ok(program) => interpret(program),
+        Err(err) => eprintln!("Troubles with file reading: {}", err)
+    }
 }
 
-fn read_file (filename: &String) -> io::Result<()> {
+fn read_file (filename: &String) -> io::Result<Vec<u8>> {
     let mut file = std::fs::File::open(filename)?;
-    let mut list_of_chunks = Vec::new();
+    let mut bytes = Vec::new();
     let chunk_size = 2;
 
     loop {
@@ -22,22 +26,46 @@ fn read_file (filename: &String) -> io::Result<()> {
         // What is adaptor?
         let n = file.by_ref().take(chunk_size as u64).read_to_end(&mut chunk)?;
         if n == 0 { break; }
-        list_of_chunks.push(chunk);
+        bytes.append(&mut chunk);
         if n < chunk_size { break; }
     }
 
-    for chunk in list_of_chunks.into_iter() {
-        println!("{:#06x}", chunk_to_u32(chunk));
-    }
-
-    Ok(())
+    Ok(bytes)
 }
 
-fn chunk_to_u32 (chunk: Vec<u8>) -> u32 {
-    let mut res: u32 = 0;
-    for byte in chunk.into_iter() {
-        res = (res << 8) + (byte as u32);
-    }
+fn interpret (program: Vec<u8>) {
+    let mut screen: [[u8; 64]; 32] = [[1; 64]; 32];
+    let mut program_counter = 0;
+    let mut index_reg = 0;
+    let mut var_regs: [u8; 16] = [0; 16];
 
-    res
+    loop {
+        let first = program[program_counter];
+        let second = program[program_counter + 1];
+        program_counter += 2;
+
+        if first == 0x00 && second == 0xe0 {
+            clear_screen(&mut screen);
+        } else if (first >> 4) == 0x1 {
+            program_counter = ((usize::from(first) - 0x10) << 8) +
+                usize::from(second);
+        } else if (first >> 4) == 0xA {
+            index_reg = ((usize::from(first) - 0xA0) << 8) +
+                usize::from(second);
+        } else if (first >> 4) == 0x6 {
+            let reg_number = usize::from(first - 0x60);
+            var_regs[reg_number] = second;
+        } else if (first >> 4) == 0x7 {
+            let reg_number = usize::from(first - 0x70);
+            var_regs[reg_number] += second;
+        }
+    }
+}
+
+fn clear_screen (screen: &mut [[u8; 64]; 32]) {
+    for line in screen.iter_mut() {
+        for px in line.iter_mut() {
+            *px = 0;
+        }
+    }
 }
